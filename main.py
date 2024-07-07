ee1179cadb7ca8b96672048e48bf8bd5ae1bebb6
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-07-08 00:15:44 +0300

    Trying crud operations with fastapi

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status, Response
 from fastapi.params import Body
+from random import randrange
 from pydantic import BaseModel
- 
+
 
 app = FastAPI()
+
+my_posts = [
+    {'id': 1, 'title': 'Python', 'content': 'Let\'s learn Python'},
+    {'id': 2, 'title': 'SQL', 'content': 'Let\'s learn SQL'}
+]
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+
+
+def get_p(id: int):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+
+
+def get_index_post(id: int):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+    return None
 
 
 @app.get('/')
@@ -11,6 +35,35 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.post('/createpost')
-def create_post(payload: dict = Body()):
-    return {"title": f"Title: {payload['title']} Content: {payload['content']}"}
+@app.get("/posts")
+def get_all_posts():
+    return {"data": my_posts}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = get_p(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail={"message": f"post with id: {id} was not found!"})
+    return {"data": f'Here is post {id}'}
+
+
+@app.post('/createpost', status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict["id"] = randrange(3, 100000)
+    my_posts.append(post_dict)
+    return {"data": f"Title: {post_dict['title']} Content: {post_dict['content']}"}
+
+
+@app.delete('/deletepost/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = get_index_post(id)
+
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} doesn't exist!")
+
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/adel-hamed/PycharmProjects/fastapi/venv"
+set -gx VIRTUAL_ENV "/home/adel-hamed/PycharmProjects/accountant/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
