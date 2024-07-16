dc0bae2e113c68b7de4da42226a1783d473d5499
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-07-16 12:37:26 +0300

    Adding query parameter functionality

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -12,8 +12,8 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schema.PostRespone])
-def get_all_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
+def get_all_posts(db: Session = Depends(get_db), limit: int = 10, skip: int = 0):
+    posts = db.query(models.Post).limit(limit).offset(skip).all()
     return posts
 
 
