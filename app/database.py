a3695e976eec5884aa4eeba1853ee789c07408fb
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-07-17 13:02:17 +0300

    Adding env variable validation

--- app/database.py
+++ app/database.py
@@ -3,8 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import psycopg
 from psycopg.rows import dict_row
+from .config import setting
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin123@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = (f"postgresql://{setting.database_username}:{setting.database_password}@"
+                           f"{setting.database_hostname}/{setting.database_name}")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
